f8006b750fdb473270a1293d9c45c4a00d4eedee
Jackal_lzt <bkyarokun@gmail.com>
2026-07-10 16:50:17 +0800

    Add whatthegit 0.1.0

---
+++ Formula/whatthegit.rb
@@ -0,0 +1,18 @@
+class Whatthegit < Formula
+  desc "Understand a Git repository before reading its code"
+  homepage "https://github.com/Bkyaro/whatthegit"
+  url "https://github.com/Bkyaro/whatthegit/releases/download/v0.1.0/whatthegit-0.1.0.tar.gz"
+  sha256 "fa15ad3195198b186cae827d106675fa25af7f1d72168933cfdb44834022e0cc"
+  license "MIT"
+
+  depends_on "git"
+
+  def install
+    bin.install "whatthegit"
+    bin.install_symlink "whatthegit" => "wtg"
+  end
+
+  test do
+    assert_match "whatthegit 0.1.0", shell_output("#{bin}/wtg --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Bkyaro Homebrew Tap
+
+Homebrew formulae for [Bkyaro](https://github.com/Bkyaro) projects.
+
+## Install whatthegit
+
+```bash
+brew install Bkyaro/tap/whatthegit
+```
+
+This installs both `whatthegit` and the short alias `wtg`.
